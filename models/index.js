fc3e812a40b2bf1cef098208796bdca63d03fa14
Kyle Larsen <kylelarsen.business@gmail.com>
2022-09-25 13:35:55 -0600

    added login, logout, and auth javascript files. created a cookie thingy.

--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
 const User = require('./User');
 const History = require('./History');
+const Wishlist = require('./Wishlist');
 
 module.exports = { User };
 module.exports = { History };
+module.exports = { Wishlist };
--- server.js
+++ server.js
@@ -10,6 +10,21 @@ const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
 
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000,
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -22,6 +37,4 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
-
-  
 
