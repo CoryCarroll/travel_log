a0ae7a5ab8a1847d9a2695e3f28ed2b88e5a559a
CorysPC <corystory2@gmail.com>
2022-09-27 19:25:06 -0600

    working at this point

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -65,10 +65,10 @@ router.get('/profile', async (req, res) => {
 
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
-  if (req.session.logged_in) {
-    res.redirect('/profile');
-    return;
-  }
+  // if (req.session.logged_in) {
+  //   res.redirect('/user');
+  //   return;
+  // }
 
   res.render('login');
 });
