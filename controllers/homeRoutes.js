a5816b7b66e20cf296b5aeca567cac3e7df0c38c
Kyle Larsen <kylelarsen.business@gmail.com>
2022-10-31 18:07:06 -0600

    added authentication to the get route at index

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { User, Wishlist, History } = require('../models');
 const withAuth = require('../utils/auth')
 
-router.get('/', async (req, res) => {
+router.get('/', withAuth, async (req, res) => {
   try {
     // Get history and wishlist data to tie to a user
     const historyData = await History.findAll({
